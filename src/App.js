7d8998d7a6f8cee9413b4507d4cef5c447205a24
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-11-25 11:28:58 +0600

    toaster added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import { RouterProvider } from "react-router";
 import "./App.css";
 import router from "./routes/Routes";
 import Footer from "./shared/Footer";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App max-w-[1440px] mx-auto h-full">
       <RouterProvider router={router}> </RouterProvider>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            border: "1px solid black",
+            padding: "10px",
+            color: "#713200",
+          },
+        }}
+      />
       <Footer></Footer>
     </div>
   );
